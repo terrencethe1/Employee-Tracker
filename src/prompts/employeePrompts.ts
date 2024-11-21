dc199f27312c5932f94a78a3e3b941886883a35e
terrencethe1 <terrence.jsr@gmail.com>
2024-11-20 23:24:29 -0600

    add main menu prompt using inquirer, Import inquirer module, Create an async function  that prompts the user with a list of actions, Return the selected action

---
+++ src/prompts/employeePrompts.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+
+export const mainMenu = async (): Promise<string> => {
+  const { action } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Exit",
+      ],
+    },
+  ]);
+  return action;
+};
